df1779de130d2f9ae5a3f132317fd2f214ec8ac1
Kemberly Eliscar <kemberlyeliscar@gmail.com>
2020-03-14 23:41:03 -0400

    added hash and time complexity to router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const Users = require("../users/users-model")
+const bcrypt = require('bcryptjs')
 
 const router = express.Router()
 
@@ -7,6 +8,7 @@ router.post("/register", async (req, res, next) => {
 	try {
 		const { username } = req.body
 		const user = await Users.findBy({ username }).first()
+		const hash = await bcrypt.hash(password, 10)
 
 		if (user) {
 			return res.status(409).json({
@@ -24,8 +26,9 @@ router.post("/login", async (req, res, next) => {
 	try {
 		const { username, password } = req.body
 		const user = await Users.findBy({ username }).first()
+		const hash = await bcrypt.hash(password, 10)
 
-		if (!user) {
+		if (!user || hash !== user.password) {
 			return res.status(401).json({
 				message: "Invalid Credentials",
 			})
