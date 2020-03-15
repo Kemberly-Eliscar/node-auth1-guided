4a8b0015c6718e3a742ac55979771c0042800ec5
Kemberly Eliscar <kemberlyeliscar@gmail.com>
2020-03-14 23:13:17 -0400

    added hash and time complexity

--- users/users-model.js
+++ users/users-model.js
@@ -1,6 +1,9 @@
+const bcrypt = require("bcryptjs")
 const db = require("../database/config")
 
 async function add(user) {
+	// has the password with a time complexity of 10
+	user.password = await bcrypt.hash(user.password, 10)
 	const [id] = await db("users").insert(user)
 	return findById(id)
 }
